a1824c153a8dd8b07120a1e35392ef99d98d237d
Sbelkos <dante7etnad@gmail.com>
2022-12-10 20:58:45 +0700

    update with eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-//import readlineSync from "readline-sync";
+// import readlineSync from "readline-sync";
 
-//const greeting = console.log("Welcome to the Brain Games!");
+// const greeting = console.log("Welcome to the Brain Games!");
 
-import { greeting, helloUser } from "../src/cli.js";
+import { greeting, helloUser } from '../src/cli.js';
 
-greeting;
-helloUser;
+greeting();
+helloUser();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-//const readlineSync = rquire("redline-sync");
+// const readlineSync = rquire("redline-sync");
 
-export const greeting = console.log("Welcome to the Brain Games!");
-const userName = readlineSync.question("May I have you name? ");
-export const helloUser = console.log("Hello, " + userName + "!");
+export const greeting = console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have you name? ');
+export const helloUser = console.log(`Hello, ${userName}!`);
 
-//export { greeting, helloUser };
+// export { greeting, helloUser };
