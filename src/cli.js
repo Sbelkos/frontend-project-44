4970ac32985c415420a11c2c3ef2296ddc0856e5
Sbelkos <dante7etnad@gmail.com>
2023-01-17 21:28:20 +0700

    Add 1-even-game.js

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,9 @@
 import readlineSync from 'readline-sync';
 
-// const readlineSync = rquire("redline-sync");
-
-export const greeting = () => {
+const userGreeting = () => {
   console.log('Welcome to the Brain Games!');
-};
-
-export const helloUser = () => {
   const userName = readlineSync.question('May I have you name? ');
   console.log(`Hello, ${userName}!`);
 };
 
-// export { greeting, helloUser };
+export default userGreeting;
