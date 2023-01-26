79072b89818b3bc91755945ac93681a3da2a03a2
Sbelkos <dante7etnad@gmail.com>
2023-01-26 20:31:38 +0700

    Add GCD game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import startBrainGCD from '../src/games/3-gcd-game.js';
+
+startBrainGCD();
---
+++ src/games/3-gcd-game.js
@@ -0,0 +1,25 @@
+import runEngine from '../index.js';
+
+import getRandomNumber from '../randomNumberGeneratre.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const calculatingGCD = (num1, num2) => {
+  if (num2 > num1) return calculatingGCD(num2, num1);
+  if (!num2) return num1;
+  return calculatingGCD(num2, num1 % num2);
+};
+
+const generateRound = () => {
+  const generateNumber1 = getRandomNumber(1, 50);
+  const generateNumber2 = getRandomNumber(1, 50);
+  const question = `${generateNumber1} ${generateNumber2}`;
+  const trueAnswer = String(calculatingGCD(generateNumber1, generateNumber2));
+  return [question, trueAnswer];
+};
+
+const startBrainGCD = () => {
+  runEngine(rules, generateRound);
+};
+
+export default startBrainGCD;
