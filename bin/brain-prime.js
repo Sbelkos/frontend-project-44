83853f3f3b20b6fd43b8a065ea27edab668155fb
Sbelkos <dante7etnad@gmail.com>
2023-01-26 21:44:16 +0700

    Add prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import startBrainPrime from '../src/games/5-prime-game.js';
+
+startBrainPrime();
---
+++ src/games/5-prime-game.js
@@ -0,0 +1,25 @@
+import runEngine from '../index.js';
+
+import getRandomNumber from '../randomNumberGeneratre.js';
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num === 0 || num === 1) return false;
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const generateRound = () => {
+  const question = getRandomNumber(1, 100);
+  const trueAnswer = isPrime(question) === true ? 'yes' : 'no';
+  return [question, trueAnswer];
+};
+
+const startBrainPrime = () => {
+  runEngine(rules, generateRound);
+};
+
+export default startBrainPrime;
