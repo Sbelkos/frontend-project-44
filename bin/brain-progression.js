52f9b619ff4233de46a51efc4561b30ef387390b
Sbelkos <dante7etnad@gmail.com>
2023-01-26 21:15:32 +0700

    Add game progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import startBrainProgression from '../src/games/4-progression-game.js';
+
+startBrainProgression();
---
+++ src/games/4-progression-game.js
@@ -0,0 +1,33 @@
+import runEngine from '../index.js';
+
+import getRandomNumber from '../randomNumberGeneratre.js';
+
+const rules = 'What number is missing in the progression?';
+
+const getProgression = (randomFirstNum, minLengthNum, randomStepNum) => {
+  const resultProgression = [];
+  for (let i = randomFirstNum; i < minLengthNum; i += randomStepNum) {
+    if (resultProgression.length !== 10) resultProgression.push(i);
+  }
+  return resultProgression;
+};
+
+const generateRound = () => {
+  const randomFirstNum = getRandomNumber(1, 10);
+  const randomStepNum = getRandomNumber(1, 5);
+  const minLengthNum = randomFirstNum + (randomStepNum * 10);
+
+  const progression = getProgression(randomFirstNum, minLengthNum, randomStepNum);
+  const hiddenIndex = getRandomNumber(0, progression.length - 1);
+  const trueAnswer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, trueAnswer];
+};
+
+const startBrainProgression = () => {
+  runEngine(rules, generateRound);
+};
+
+export default startBrainProgression;
